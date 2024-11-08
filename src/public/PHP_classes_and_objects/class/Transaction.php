7bf75608a02f6896f47047f0359002717ae4540c
berhan <berhan5378@gmail.com>
2024-11-08 15:57:49 +0300

    Transaction class

---
+++ src/public/PHP_classes_and_objects/class/Transaction.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types = 1);
+
+class Transaction
+{
+    public $amount;
+    public $description;
+
+    public float $amount1=20;
+    public float $amount2; 
+
+    private $amount3=50;
+
+    
+    public function addTax(float $rate)
+    {
+        $this->amount += ($this->amount * $rate) /100;
+    }
+
+    public function addTax2(float $rate):Transaction
+    {
+        $this->amount += ($this->amount * $rate) /100;
+        return $this;
+    }
+
+    public function applyDiscount(float $rate)
+    {
+        $this->amount -= ($this->amount * $rate) /100;
+    }
+
+    public function applyDiscount2(float $rate):Transaction
+    {
+        $this->amount -= ($this->amount * $rate) /100;
+        return $this;
+    }
+
+    public function getAmount():float
+    {
+       return $this->amount3;
+    }
+ 
+}
