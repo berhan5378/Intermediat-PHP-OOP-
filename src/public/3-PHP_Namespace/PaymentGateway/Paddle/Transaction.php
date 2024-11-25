5ca4f0babcc4a2fbd979e841d927483b39374677
berhan <berhan5378@gmail.com>
2024-11-25 14:20:52 +0300

    class

--- src/public/3-PHP_Namespace/PaymentGateway/Paddle/Transaction.php
+++ src/public/3-PHP_Namespace/PaymentGateway/Paddle/Transaction.php
@@ -13,13 +13,9 @@ class Transaction
     {
         var_dump(new CustomerProfile());
         //var_dump(new DateTime());//error b/n DateTime() is not found
-        var_dump(new \DateTime()); // it's work properly b/c it's load from global namespace not local the reason of '/'
+        var_dump(new \DateTime()); // it's work properly b/c it's load from global namespace not local the reason of '/' ( we use fullyqualified name if we were not use 'use' )
         var_dump(new DateTime());//it's work properly b/c we used 'use DateTime;'
         var_dump(new \Notification\Email());//it's work b/c we use fully qualified name by set this('\') on fornt of it
         var_dump(new Email());//it's work b/c we used 'use Notification\Email;'
-       
-    
-    
-    
     }
 }
